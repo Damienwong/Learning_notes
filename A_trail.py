915bcd241206d1794c5f118cdc055e37886cf135
wanghaibo <wanghaibo@hesaitech.com>
2023-02-20 14:28:35 +0800

    update

--- A_trail.py
+++ A_trail.py
@@ -500,27 +500,31 @@ import heapq
 # users = [User('Jack', 12), User('Tom', 32), User('Harry', 32), User('Emma', 12)]
 # print(users.sort(key=attrgetter('user_id')))
 
-rows = [
-    {'address': '5412 N CLARK', 'date': '07/01/2012'},
-    {'address': '5148 N CLARK', 'date': '07/04/2012'},
-    {'address': '5800 E 58TH', 'date': '07/02/2012'},
-    {'address': '2122 N CLARK', 'date': '07/03/2012'},
-    {'address': '5645 N RAVENSWOOD', 'date': '07/02/2012'},
-    {'address': '1060 W ADDISON', 'date': '07/02/2012'},
-    {'address': '4801 N BROADWAY', 'date': '07/01/2012'},
-    {'address': '1039 W GRANVILLE', 'date': '07/04/2012'},
-]
+# rows = [
+#     {'address': '5412 N CLARK', 'date': '07/01/2012'},
+#     {'address': '5148 N CLARK', 'date': '07/04/2012'},
+#     {'address': '5800 E 58TH', 'date': '07/02/2012'},
+#     {'address': '2122 N CLARK', 'date': '07/03/2012'},
+#     {'address': '5645 N RAVENSWOOD', 'date': '07/02/2012'},
+#     {'address': '1060 W ADDISON', 'date': '07/02/2012'},
+#     {'address': '4801 N BROADWAY', 'date': '07/01/2012'},
+#     {'address': '1039 W GRANVILLE', 'date': '07/04/2012'},
+# ]
+#
+# from operator import itemgetter
+# from itertools import groupby
+#
+# rows.sort(key=itemgetter('date'))
+# print(rows)
+#
+# for data, items in groupby(rows, key=itemgetter('date')):
+#     print(data)
+#     for item in items:
+#         print(item)
 
-from operator import itemgetter
-from itertools import groupby
-
-rows.sort(key=itemgetter('date'))
-print(rows)
-
-for data, items in groupby(rows, key=itemgetter('date')):
-    print(data)
-    for item in items:
-        print(item)
-
+import os
+import psutil
+pid = os.getpid()
+print(pid)
 
 
